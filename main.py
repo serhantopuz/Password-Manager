202e580fa09c2a1f34af20af3e4958680b2157f9
serhantopuz <serhantpz123@gmail.com>
2023-10-04 12:58:54 +0300

    adding json and error handling

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
-
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
 def generate_password():
     password_output.delete(0, END)
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
@@ -28,24 +30,55 @@ def generate_password():
                               # when you click the generate password button
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def save_docs():
-    username = username_input.get()
-    website_name = website_input.get()
-    user_password = password_output.get()
 
-    if len(username) == 0 or len(user_password) == 0:
+
+def save_docs():
+    user_mail = user_mail_input.get()
+    website_name = website_input.get().title()
+    user_password = password_output.get()
+    new_data = {website_name:
+                    {
+                        "email": user_mail,
+                        "password": user_password
+                    }
+                }
+
+    if len(user_mail) == 0 or len(user_password) == 0:
         messagebox.showinfo(title="Ooops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website_name,
-                                       message=f"These are the details entered: \nWebsite Name: {website_name} "
-                                               f"\nEmail: {username} \nPassword: {user_password} \nIs it OK to save? ")
-        if is_ok:
-            with open("passwords.txt", "a") as file:
-                file.write(f"{website_name}  |  {username}  |  {user_password}\n")
-                website_input.delete(0, END)
-                password_output.delete(0, END)
+        try:
+            with open("passwords.json", "r") as file:
+                data = json.load(file)
+                data.update(new_data)
+        except FileNotFoundError:
+            with open("passwords.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            with open("passwords.json", "w") as file:
+                json.dump(data, file, indent=4)
+        finally:
+            website_input.delete(0, END)
+            password_output.delete(0, END)
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
 
 
+def search():
+    given_website = website_input.get().title()
+    if len(given_website) == 0:
+        messagebox.showinfo(title="Ooops", message="Please don't leave any fields empty!")
+    else:
+        try:
+            with open("passwords.json", "r") as file:
+                data = json.load(file)
+                message = ""
+                for key, value in data[given_website].items():
+                    message += f"{key}: {value}\n"
+                messagebox.showinfo(title=given_website, message=message)
+        except FileNotFoundError:
+            messagebox.showerror(title="Error", message="No data file found.")
+        except KeyError:
+            messagebox.showerror(title="Error", message="No details for the website exists.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -70,13 +103,13 @@ password_text = Label(text="Password:")
 password_text.grid(row=3, column=0)
 
 # Entries
-website_input = Entry(width=41)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input = Entry(width=25)
+website_input.grid(row=1, column=1)
 website_input.focus()
 
-username_input = Entry(width=41)
-username_input.grid(row=2, column=1, columnspan=2)
-username_input.insert(0, "serhan@gmail.com")  # default value for starting the program when you use usually same mail
+user_mail_input = Entry(width=41)
+user_mail_input.grid(row=2, column=1, columnspan=2)
+user_mail_input.insert(0, "serhan@gmail.com")  # default value for starting the program when you use usually same mail
 
 password_output = Entry(width=25)
 password_output.grid(row=3, column=1)
@@ -84,6 +117,9 @@ password_output.grid(row=3, column=1)
 # Buttons
 generate_button = Button(text="Generate Password", width=12, command=generate_password)
 generate_button.grid(row=3, column=2)
+
+search_button = Button(text="Search", width=12, command=search)
+search_button.grid(row=1, column=2)
 
 add_button = Button(text="add", width=40, command=save_docs)
 add_button.grid(row=4, column=1, columnspan=2)
